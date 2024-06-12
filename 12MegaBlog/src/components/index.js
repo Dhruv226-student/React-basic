2f8d05804d4f1cd77a8606c30a3a8d74692cde12
Dhruv <pateldhruv5536@gmail.com>
2024-06-12 17:24:13 +0530

    react-hook-form

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -3,10 +3,14 @@ import Footer from './Footer.jsx/Footer';
 import Container from './Container/Container';
 import Logo from './Logo';
 import LogoutBtn from './Header.js/LogoutBtn';
+import Button from './Button';
+import Input from './Input';
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Button,
+    Input,
 }
